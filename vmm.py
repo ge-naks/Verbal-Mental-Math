fc4200c4a19911c0b40fbff9d9a42654a73215df
ge-naks <georgewnakhla@gmail.com>
2023-05-17 15:06:44 -0400

    Added functionality to understand and process speech, compare it with actual answer, and determine if given answer is correct or incorrect
    Success!

--- vmm.py
+++ vmm.py
@@ -1,62 +1,79 @@
 
 import pyttsx3
 import random
-import pyaudio
-import requests
-import json
-import time
-import wave
-import sys
-
-
+from playsound import playsound
+import speech_recognition as sr
+import playsound
+import tkinter
 
 engine = pyttsx3.init()
-
-p = pyaudio.PyAudio()
-stream = p.open(
-    frames_per_buffer= 3200,
-    rate=16000,
-    format= pyaudio.paInt16,
-    channels= 1,
-    input= True,
-
-)
-
-
 operators = ['plus', 'minus', 'times', 'divided by']
-for i in range(0,4):
-    num1Addition = random.randrange(2,100)
-    num2Addition = random.randrange(2,100)
+
+num1Addition = random.randrange(2,100)
+num2Addition = random.randrange(2,100)
 
 
-    num1Multiplication = random.randrange(2,12)
-    num2Multiplication = random.randrange(2,100)
+num1Multiplication = random.randrange(2,12)
+num2Multiplication = random.randrange(2,100)
 
-    op =  random.randrange(0,4)
-    if op == 0:
+op =  random.randrange(0,4)
+if op == 0:
+    realans = num1Addition + num2Addition
+    engine.say(num1Addition)
+    engine.say(operators[op])
+    engine.say(num2Addition)
+elif op == 2:
+    realans = num1Multiplication * num2Multiplication
+    engine.say(num1Multiplication)
+    engine.say(operators[op])
+    engine.say(num2Multiplication)
+elif op == 1:
+    if num1Addition > num2Addition:
+        realans = num1Addition - num2Addition
         engine.say(num1Addition)
         engine.say(operators[op])
         engine.say(num2Addition)
-    elif op == 2:
-        engine.say(num1Multiplication)
-        engine.say(operators[op])
-        engine.say(num2Multiplication)
-    elif op == 1:
-        if num1Addition > num2Addition:
-            engine.say(num1Addition)
-            engine.say(operators[op])
-            engine.say(num2Addition)
-        else:
-            engine.say(num2Addition)
-            engine.say(operators[op])
-            engine.say(num1Addition)
     else:
-        if num2Multiplication % num1Multiplication != 0:
-            while num2Multiplication % num1Multiplication != 0:
-                num1Multiplication = random.randrange(2,12)
-                num2Multiplication = random.randrange(2,100)
-            engine.say(num2Multiplication)
-            engine.say(operators[op])
-            engine.say(num1Multiplication)
+        realans =  num2Addition - num1Addition
+        engine.say(num2Addition)
+        engine.say(operators[op])
+        engine.say(num1Addition)
+else:
+    if num2Multiplication % num1Multiplication != 0:
+        while num2Multiplication % num1Multiplication != 0:
+            num1Multiplication = random.randrange(2,12)
+            num2Multiplication = random.randrange(2,100)
+        realans =  num2Multiplication / num1Multiplication
+        engine.say(num2Multiplication)
+        engine.say(operators[op])
+        engine.say(num1Multiplication)
 
-engine.runAndWait()
+engine.runAndWait() 
+
+r = sr.Recognizer()
+
+
+API_KEY = "AIzaSyAh_qQGUQ_eT7gGyHdGJwXB8MBSNCEJzRA"
+
+def main():
+    with sr.Microphone() as source:
+        print("speak")
+        audio = r.listen(source)
+        try:
+            answer = r.recognize_google(audio)
+            
+        except Exception as e:
+            print("error:" + str(e))
+
+    print(answer)
+    if int(answer) == realans:
+        print("correct")
+        playsound.playsound('right.wav')
+        engine.runAndWait() 
+    else:
+        print("wrong dummy")
+        playsound.playsound('wrong.wav')
+        engine.runAndWait() 
+main()
+
+
