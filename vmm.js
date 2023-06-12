d903e5583cf27df0bb7da5f946f1914551ff66f4
ge-naks <georgewnakhla@gmail.com>
2023-06-11 22:50:25 -0400

    js

--- vmm.js
+++ vmm.js
@@ -1,57 +1,88 @@
-// function speak(text){
-//     let utterance = new SpeechSynthesisUtterance(text)
-//     let ss = new speechSynthesis
-//     speechSynthesis.speak(utterance)
-// }
+
+
+operators = ['plus', 'minus', 'times', 'divided by']
+transcript = ""
+duration = 10
+running = true
+test = -1;
+
+let soundCorrect = new Audio ("right.wav")
+let soundIncorrect = new Audio ("wrong.wav")
+
+numCorrect = 0
+numIncorrect = 0
+
+
+function countdown(duration) {
+    const timer = setInterval(() => {
+      if (seconds > 0) {
+        console.log()
+        seconds--
+      } else {
+        running = false
+        console.log("Countdown completed.")
+        clearInterval(timer)
+      }
+    }, 1000);
+  }
+  
+  function listen() {
+    SpeechRecognition.
+    
+  }
+
+function speak(num1, num2, i){
+        let firstNum = new SpeechSynthesisUtterance(String(num1))
+        speechSynthesis.speak(firstNum);
+
+        let op  =  new SpeechSynthesisUtterance(operators[i])
+        speechSynthesis.speak(op);
+
+        let secondNum = new SpeechSynthesisUtterance(String(num2))
+        speechSynthesis.speak(secondNum)
+ }
+
 
 function genNumber(){
-    num1Addition = Math.random(2,100)
-    num2Addition = Math.random(2,100)
+    num1Addition = Math.floor(Math.random() * (100 - 2 + 1)) + 2;
+    num2Addition = Math.floor(Math.random() * (100 - 2 + 1)) + 2;
 
-    num1Multiplication = Math.random(2,12)
-    num2Multiplication = Math.random(2,100)
+    num1Multiplication = Math.floor(Math.random() * (12 - 2 + 1)) + 2;
+    num2Multiplication = Math.floor(Math.random() * (100 - 2 + 1)) + 2;
 
-    op =  Math.random(0,4)
+    op = Math.floor(Math.random() * (4 - 2 + 1)) + 0;
     if (op == 0){
-        test = num1Addition + num2Addition
-        let utterance = new SpeechSynthesisUtterance(num1Addition);
-        speechSynthesis.speak(utterance);
-
-        utterance = new SpeechSynthesisUtterance(num2Addition);
-        speechSynthesis.speak(utterance);
-        
-        engine.say(num1Addition)
-        engine.say(operators[op])
-        engine.say(num2Addition)
+        test = num1Addition + num2Addition  
+        speak(num1Addition, num2Addition, 0)
     }    
     else if(op == 2){
         test = num1Multiplication * num2Multiplication
-        engine.say(num1Multiplication)
-        engine.say(operators[op])
-        engine.say(num2Multiplication)
+
+        speak(num1Multiplication, num2Multiplication, 2)
 
     } else if(op == 1){
         if (num1Addition > num2Addition){
             test = num1Addition - num2Addition
-            engine.say(num1Addition)
-            engine.say(operators[op])
-            engine.say(num2Addition)
+            speak(num1Addition, num2Addition, 1)
                 } else{
-                    test = num2Addition - num1Addition
-                    engine.say(num2Addition)
-                    engine.say(operators[op])
-                    engine.say(num1Addition)
+                    speak(num2Addition, num1Addition, 1)
                 }
     }else{
-        if (num2Multiplication % num1Multiplication != 0)
-        while (num2Multiplication % num1Multiplication != 0)
-            num1Multiplication = random.randrange(2,12)
-            num2Multiplication = random.randrange(2,100)
-        test = num2Multiplication / num1Multiplication
-        engine.say(num2Multiplication)
-        engine.say(operators[op])
-        engine.say(num1Multiplication)
+        if(num2Multiplication % num1Multiplication != 0){
+            while (num2Multiplication % num1Multiplication != 0){
+                console.log("here4")
+                num1Multiplication = Math.floor(Math.random() * (12 - 2 + 1)) + 2;
+                num2Multiplication = Math.floor(Math.random() * (100 - 2 + 1)) + 2;
+            }
+           test = num2Multiplication / num1Multiplication
+           speak(num2Multiplication, num1Multiplication, 3)
+        }
+        
     }        
+
+}
+
+function main(){
 
 }
 
